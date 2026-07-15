f581e36e91aadab67adf2850825470e8e903a339
Monsora Cueto <jonathancueto2001@gmail.com>
2026-07-15 12:03:30 -0500

    monsra

---
+++ MonsoraTest.swift
@@ -0,0 +1,18 @@
+//
+//  MonsoraTest.swift
+//  
+//
+//  Created by DPI Student 024 on 7/15/26.
+//
+
+import SwiftUI
+
+struct MonsoraTest: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MonsoraTest()
+}
