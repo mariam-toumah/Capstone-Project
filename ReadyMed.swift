d3dfcf552ca301d283ebbaee5d1a344e745da47d
joyopareameyaw <opareameyawjoy@gmail.com>
2026-07-15 12:05:03 -0500

    joy3

---
+++ ReadyMed.swift
@@ -0,0 +1,18 @@
+//
+//  ReadyMed.swift
+//  
+//
+//  Created by DPI Student 020 on 7/15/26.
+//
+
+import SwiftUI
+
+struct ReadyMed: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReadyMed()
+}
